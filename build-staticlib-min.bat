cbbd371844951c50edae3a6de7851211153c0290
William Tremblay <tremwil@gmail.com>
2025-03-11 00:58:59 +0000

    update staticlib build scripts

---
+++ build-staticlib-min.bat
@@ -0,0 +1,2 @@
+set RUSTFLAGS=-Zlocation-detail=none -Zfmt-debug=none
+cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features="optimize_for_size" -p arxan-disabler -F disabler,ffi --release
--- build-staticlib-min.sh
+++ build-staticlib-min.sh
@@ -1,5 +1,6 @@
 RUSTFLAGS="-Zlocation-detail=none -Zfmt-debug=none" cargo +nightly build \
 	-Z build-std=std,panic_abort \
 	-Z build-std-features="optimize_for_size" \
-	-F disabler ffi \
+	-p arxan-disabler \
+	-F disabler,ffi \
 	--release
