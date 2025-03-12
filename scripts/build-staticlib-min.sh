bbaf62ac3bd25ef1326c6860fef98053170c4cfc
William Tremblay <tremwil@gmail.com>
2025-03-12 21:24:57 +0000

    forward args in staticlib build script

--- scripts/build-staticlib-min.sh
+++ scripts/build-staticlib-min.sh
@@ -2,5 +2,4 @@ RUSTFLAGS="-Zlocation-detail=none -Zfmt-debug=none" cargo +nightly build \
 	-Z build-std=std,panic_abort \
 	-Z build-std-features="optimize_for_size" \
 	-p arxan-disabler \
-	-F disabler,ffi \
-	--release
+	--release $@
