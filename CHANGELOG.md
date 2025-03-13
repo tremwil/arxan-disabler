1bbd51adc2e8730e86fffe32110f074d41a2ca1c
William Tremblay <tremwil@gmail.com>
2025-03-13 16:02:32 +0000

    add changelog to release notes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [0.1.2] - 2025-03-13
 
-### Fixed:
+### Added:
 - CICD release pipeline should now create a release with changelog
 
 ## [0.1.1] - 2025-03-13
---
+++ scripts/gen-changelog.py
@@ -0,0 +1,12 @@
+import os, json, re
+
+cargo_meta = json.loads(os.popen("cargo metadata --format-version 1 -q").read())
+version: str = next(p["version"] for p in cargo_meta["packages"] if p["name"] == "arxan-disabler")
+
+with open("CHANGELOG.md", "r") as f:
+    changelog = f.read()
+
+regex = re.compile(rf"\[{version.replace(".", "\\.")}][^\n]*\n+(.*?)\n\#\# ", re.S)
+changes = next(regex.finditer(changelog)).group(1)
+
+print(changes)
